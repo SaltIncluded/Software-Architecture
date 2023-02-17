8a3773dea121c7bc68b18ec9287fd97d2dc2dd4d
SaltIncluded <casperfj2011@gmail.com>
2023-02-17 20:16:52 +0100

    exercise_2

---
+++ exercises_lecture02/dotnet_consumer/dotnetConsumer/dotnetConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the bootstrap server is the address of the kafka broker, i.e. the docker container. Here you can specify multiple brokers 
+            // separated by a comma to enable load balancing and fault tolerance.
+            // The group id is used to identify the consumer group. All consumers with the same group id belong to the same consumer group.
+            // The auto offset reset defines the behavior of the consumer if no offset is stored for a partition. Here we set it to earliest
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "kafka1:9092",
+                GroupId = "example-consumer-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("example_topic");
+
+                Console.WriteLine("Kafka Consumer is running...");
+
+                while (true)
+                {
+                    try
+                    {
+                        var message = consumer.Consume();
+
+                        Console.WriteLine($"Message received: '{message.Value}' from partition {message.Partition} with offset {message.Offset}");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Consumption failed: {e.Error.Reason}");
+                    }
+                }
+            }
+        }
+    }
+}
